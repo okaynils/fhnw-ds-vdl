8eca333922a4671907a6196c0a7d7ab43842b34b
okaynils <nils.fahrni@students.fhnw.ch>
2025-01-01 16:56:14 +0100

    added first analyzer draft

--- utils.py
+++ utils.py
@@ -1,8 +1,8 @@
 import torch
 
 def unnormalize(img, mean, std):
-    mean = torch.tensor(mean).view(-1, 1, 1)  # Reshape to [C, 1, 1]
-    std = torch.tensor(std).view(-1, 1, 1)    # Reshape to [C, 1, 1]
+    mean = torch.tensor(mean).view(-1, 1, 1)
+    std = torch.tensor(std).view(-1, 1, 1)
     img = std * img + mean
     img = torch.clip(img, 0, 1)
     return img
