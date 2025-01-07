7c2e717529da2bca906a2eadbd61c2fd479576fe
Nils <nils.fahrni@students.fhnw.ch>
2025-01-07 17:57:16 +0100

    added further descriptions to experiments

--- metrics.py
+++ metrics.py
@@ -21,9 +21,8 @@ def calculate_psnr(img1, img2):
     Returns:
         float: Average PSNR value for the batch.
     """
-    # bring images into sample domain
     img1 = (img1.clamp(-1, 1) + 1) / 2
-    img1 = (img1 * 255).type(torch.float16) # float for mse calculation
+    img1 = (img1 * 255).type(torch.float16)
     
     max_pixel_value = max(img1.max(), img2.max())
 
@@ -45,18 +44,15 @@ def calculate_fid(real_images, generated_images, device):
     Returns:
         float: FID score.
     """
-    # Load pre-trained Inception model
     inception = inception_v3(init_weights=True, transform_input=False).to(device)
     inception.eval()
 
     real_features = get_features(real_images, inception_model=inception)
     gen_features = get_features(generated_images, inception_model=inception)
 
-    # Compute statistics
     mu_real, sigma_real = real_features.mean(axis=0), np.cov(real_features, rowvar=False)
     mu_gen, sigma_gen = gen_features.mean(axis=0), np.cov(gen_features, rowvar=False)
 
-    # FID calculation
     diff = mu_real - mu_gen
     covmean, _ = sqrtm(sigma_real.dot(sigma_gen), disp=False)
     if np.iscomplexobj(covmean):
